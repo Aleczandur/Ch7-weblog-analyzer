33a8d9bf3f5847d9721f0eae40e6a8b72d88b3b4
Aleczandur <43154682+Aleczandur@users.noreply.github.com>
2018-10-22 14:52:38 -0400

    Excercise pages262-264
    Added code  for weblog analyzer

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,8 +1,8 @@
 /**
  * Read web server data and analyse hourly access patterns.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version    2016.02.29
+ * @author Alex Floyd
+ * @version    10.22.2018
  */
 public class LogAnalyzer
 {
@@ -14,15 +14,85 @@ public class LogAnalyzer
     /**
      * Create an object to analyze hourly web accesses.
      */
-    public LogAnalyzer()
+    public LogAnalyzer(String names)
     { 
         // Create the array object to hold the hourly
         // access counts.
         hourCounts = new int[24];
         // Create the reader to obtain the data.
-        reader = new LogfileReader();
+        reader = new LogfileReader(names);
     }
-
+    
+    public  int numAccess(){
+    int access = 0;
+    for( int check = 0; check < hourCounts.length; ++check){
+        access += hourCounts[check];
+    }
+    return access;
+    }
+    
+    public int busiestHour(){
+        int maxHour, maxCounts = 0;
+        for( int hour = 0; hour < hourCounts.length; ++ hour){
+            if(hourCounts[hour] > maxCounts)
+            {
+               maxCounts = hourCounts[hour];
+               maxHour = hour;
+            }
+        }
+        return maxCounts;
+    }
+    
+    public int quietestHour(){
+        int minHour, minCounts = 0;
+        for( int hour = 0; hour < hourCounts.length; ++ hour){
+            if (minCounts < hourCounts.length){
+               minCounts = hourCounts[hour];
+               minHour = hour;
+            }
+            else if (minCounts > hourCounts[hour] || minCounts == hourCounts[hour]){
+                minCounts = hourCounts[hour];
+            }
+        }
+        return minCounts;
+    }
+    
+    public int busiestTwoHour(){
+        int[] two_hours = new int[2];
+        int count, bigTime = 0;
+        for(count = 0; count < hourCounts.length; ++count){
+            if (bigTime > hourCounts[count]){
+                bigTime = bigTime;
+            }
+            else if (bigTime < hourCounts[count]){
+                bigTime = hourCounts[count];
+                two_hours[0] = bigTime;
+                if(count == 0){
+                    if (hourCounts[count] > hourCounts[count+1]){
+                        two_hours[0] = hourCounts[count];
+                    }
+                    else if (hourCounts[count] < hourCounts[count+1]){
+                        two_hours[0] = hourCounts[count+1];
+                    }
+                }
+                else{
+                    if(hourCounts[count+1] > hourCounts[count-1]){
+                        two_hours[1] = hourCounts[count-1];
+                    }
+                    else if (hourCounts[count+1] < hourCounts[count-1]){
+                        two_hours[0] = hourCounts[count-1];
+                        two_hours[1] = bigTime;
+                    }
+                }
+            }
+        }
+        return two_hours[0];
+    }
+                
+    
+    
+    
+    
     /**
      * Analyze the hourly access data from the log file.
      */
@@ -34,7 +104,13 @@ public class LogAnalyzer
             hourCounts[hour]++;
         }
     }
-
+    
+    
+    
+    
+    
+    
+    
     /**
      * Print the hourly counts.
      * These should have been set with a prior
